409965c4129135b3924eb358fc42f0022570139f
2287963892 <www.2287963892@qq.com>
2019-07-02 08:17:17 +0800

    我的第一次上传

--- test/testapp.js
+++ test/testapp.js
@@ -3,6 +3,7 @@ const bodyParse=require('body-parser');
 const userRouter=require('./routes/user');
 var app=express();
 app.listen(8080);
+//监听8080端口
 console.log('服务器开启');
 app.use(express.static('./public'));
 app.use(bodyParse.urlencoded({
